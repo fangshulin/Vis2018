d2925dbad105c458a8a4b3f78f0a6756940d6c54
Fangshu Lin <32018456+fangshulin@users.noreply.github.com>
2018-11-20 22:09:41 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, Response, render_template,url_for, flash, jsonify
-# from setting import host, database, user, password, Gentrifying, Non_Gentrifying, Higher_Income, cur
 import pandas as pd
 import urllib, json
 import urllib3
@@ -9,19 +8,10 @@ app = Flask(__name__)
 url = "https://raw.githubusercontent.com/fangshulin/Vis2018/master/dataset/education_expend.json"
 response = urllib.request.urlopen(url)
 data = json.loads(response.read())
-# print data
-#use urllib3
-# http = urllib3.PoolManager()
-# r = http.request('GET','https://raw.githubusercontent.com/davidhhk1994/Data-Visualization/master/final_project/New_Dataset/education_expend.json')
-# data = json.loads(r.data.decode('utf-8'))
+
 url2 = "https://raw.githubusercontent.com/fangshulin/Vis2018/master/dataset/literacy.json"
 response2 = urllib.request.urlopen(url2)
 data1 = json.loads(response2.read())
-
-
-# http = urllib3.PoolManager()
-# r2 = http.request('GET','https://raw.githubusercontent.com/davidhhk1994/Data-Visualization/master/final_project/New_Dataset/literacy.json')
-# data1 = json.loads(r2.data.decode('utf-8'))
 
 @app.route('/')
 def index():
